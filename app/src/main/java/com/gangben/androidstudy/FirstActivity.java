c9092c1dd8b4ae8fb8dc712133eaec763314cbde
xiaoyonghu <2826755215@qq.com>
2021-06-21 14:22:56 +0800

    使用intent携带数据传递到第二个act

--- app/src/main/java/com/gangben/androidstudy/FirstActivity.java
+++ app/src/main/java/com/gangben/androidstudy/FirstActivity.java
@@ -27,32 +27,35 @@ public class FirstActivity extends AppCompatActivity {
             //显式intent
             //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
 
-            //隐式intent 靠action 1 和category n 来匹配
+            //隐式intent 依据action 1 和category n 来匹配
             //Intent intent = new Intent("com.gangben.androidstudy.ACTION_START");
             //intent.addCategory("com.gangben.androidstudy.MY_CATEGORY");
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
+            //书中有写到android中的协议形式，打开电话拨号界面
+            //intent.setData(Uri.parse("tel:10086"));
 
-            //书中有写到android中的协议形式
-            intent.setData(Uri.parse("tel:10086"));
+            //使用intent传递数据
+            String data = "you see see you";
+            Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+            intent.putExtra("data", data);
             startActivity(intent);
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.add_item:
-                Toast.makeText(this,"点击了添加按钮",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "点击了添加按钮", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.remove_item:
-                Toast.makeText(this,"点击了删除按钮",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "点击了删除按钮", Toast.LENGTH_SHORT).show();
                 break;
             default:
         }
--- app/src/main/java/com/gangben/androidstudy/SecondActivity.java
+++ app/src/main/java/com/gangben/androidstudy/SecondActivity.java
@@ -2,7 +2,9 @@ package com.gangben.androidstudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        //是什么类型就用什么类型，这里是String类型
+        String msg = intent.getStringExtra("data");
+        Log.d("SecondActivity",msg);
     }
 }
