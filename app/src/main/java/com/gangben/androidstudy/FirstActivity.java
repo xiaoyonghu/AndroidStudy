43138c32b55134714014585011d25a913121c439
xiaoyonghu <2826755215@qq.com>
2021-06-21 12:41:10 +0800

    添加菜单

--- app/src/main/java/com/gangben/androidstudy/FirstActivity.java
+++ app/src/main/java/com/gangben/androidstudy/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.gangben.androidstudy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,5 +20,25 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(v -> {
             Toast.makeText(FirstActivity.this, "点击完成", Toast.LENGTH_LONG).show();
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"点击了添加按钮",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"点击了删除按钮",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
