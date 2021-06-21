3f4a068f213922795c5b07b70ac90f8213d654d2
xiaoyonghu <2826755215@qq.com>
2021-06-21 15:57:59 +0800

    使用edit_text控件

--- app/src/main/java/com/gangben/androidstudy/SecondActivity.java
+++ app/src/main/java/com/gangben/androidstudy/SecondActivity.java
@@ -5,17 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent intent = getIntent();
-        //是什么类型就用什么类型，这里是String类型
-        String msg = intent.getStringExtra("data");
-        Log.d("SecondActivity",msg);
+        //使用第二种监听方式
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(this);
+
+//        Intent intent = getIntent();
+//        //是什么类型就用什么类型，这里是String类型
+//        String msg = intent.getStringExtra("data");
+//        Log.d("SecondActivity",msg);
+
+        editText = (EditText) findViewById(R.id.edi_1);
+
+    }
+
+    /***
+     * 第二种实现点击 监控的方法
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_2:
+                //业务逻辑
+                String inputText = editText.getText().toString();
+                Toast.makeText(SecondActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
