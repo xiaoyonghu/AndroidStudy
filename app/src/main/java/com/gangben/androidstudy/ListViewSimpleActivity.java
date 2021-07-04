4cd4f7a09f6c27d678d661f0328a1737c29b5e63
xiaoyonghu <2826755215@qq.com>
2021-07-04 14:16:24 +0800

    为ListView的item设置响应事件

--- app/src/main/java/com/gangben/androidstudy/ListViewSimpleActivity.java
+++ app/src/main/java/com/gangben/androidstudy/ListViewSimpleActivity.java
@@ -3,12 +3,18 @@ package com.gangben.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 用来测试ListView的响应事件
+ */
 public class ListViewSimpleActivity extends AppCompatActivity {
 
     private List<Car> carList = new ArrayList<>();
@@ -36,6 +42,13 @@ public class ListViewSimpleActivity extends AppCompatActivity {
         CarAdapter carAdapter = new CarAdapter(ListViewSimpleActivity.this, R.layout.car_item, carList);
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(carAdapter);
+
+        //为每个item设置监听事件
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Car carCur = carList.get(position);
+            //此处只是简单的展示一下即可
+            Toast.makeText(ListViewSimpleActivity.this, carCur.getName(), Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void initCars() {
