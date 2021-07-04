6af192bf4e0b746616badeba5d17c754d37ca1d8
xiaoyonghu <2826755215@qq.com>
2021-07-04 14:41:07 +0800

    广播下的网络变化

---
+++ app/src/main/java/com/gangben/androidstudy/BroadCastNetwork.java
@@ -0,0 +1,35 @@
+package com.gangben.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class BroadCastNetwork extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast_network);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "network changed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
